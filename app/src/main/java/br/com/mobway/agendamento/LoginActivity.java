6e17f5a610c370a5e38f5f9c036beb2f24d670af
arthurcordova <arthur.stapassoli@gmail.com>
2017-09-29 12:54:39 -0300

    Foi removido a linha super.onCreate(savedInstanceState); esta nunca deve ser removida.

--- app/src/main/java/br/com/mobway/agendamento/LoginActivity.java
+++ app/src/main/java/br/com/mobway/agendamento/LoginActivity.java
@@ -17,6 +17,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
